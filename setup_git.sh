7c92841176ab414704a9e7dc2ae104f094ee2567
Sam Kim <samkim90274@gmail.com>
2020-04-17 22:38:26 -0400

    Git scripts

--- setup_git.sh
+++ setup_git.sh
@@ -1,4 +1,5 @@
+touch $HOME/.gitconfig
 git config --global user.name "Sam Kim"
-git config --global user.email "samuelkim523@gmail.com"
+git config --global user.email "samkim90274@gmail.com"
 git config --global core.editor vim
-git config --global core.excludesfile ~/.gitignore_global
+git config --global core.excludesfile $HOME/.gitignore_global
